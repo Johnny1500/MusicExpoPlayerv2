76eefab663450b5e99e6ae0f8d55f2df4fe1da2d
Andrey Kuravlev <sector1200@gmail.com>
2020-05-12 17:02:49 +0500

    small fix

--- redux/mediaActions.js
+++ redux/mediaActions.js
@@ -4,47 +4,6 @@ import axios from "axios";
 axios.defaults.baseURL =
   "https://europe-west1-media-b0cc7.cloudfunctions.net/api";
 
-// export const setTracks = () => {
-//   console.log('test1 setTracks');
-// }
-
-// export function setTracks() {
-//   console.log('test3 setTracks');
-  
-//   return (dispatch) => {
-//     console.log('test2 setTracks');
-//     dispatch({ type: LOADING_DATA });
-//     axios.get("https://europe-west1-media-b0cc7.cloudfunctions.net/api/tracks/").then((res) => {
-//       dispatch({ type: SET_TRACKS, payload: res.data });
-//     })
-//     .catch((err) => console.log("err:", err));
-//   };
-// }
-
-// const tracks = {
-//   "1": { author: "May Gillington Byron", title: "A Day With John Milton" },
-// };
-
-// export const setTracks = async (dispatch) => {
-//   console.log("test setTracks");
-
-//   try {
-//     dispatch({ type: LOADING_DATA });
-
-//     let res = await new Promise((resolve) => {
-//       setTimeout(() => {
-//         resolve(tracks);
-//       }, 1000);
-//     });
-
-//     console.log("res :>> ", res);
-
-//     dispatch({ type: SET_TRACKS, payload: res });
-//   } catch (e) {
-//     dispatch({ type: LOADING_FAILED, message: e.message });
-//   }
-// };
-
 export const setTracks = () => async (dispatch) => {
 
   // console.log('test1 setTracks');
