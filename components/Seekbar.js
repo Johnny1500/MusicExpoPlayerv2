6ee61c5d06a1f98474921a87388fe2ec8acc880c
Andrey Kuravlev <sector1200@gmail.com>
2020-05-29 13:29:57 +0500

    Slider part 2

--- components/Seekbar.js
+++ components/Seekbar.js
@@ -7,12 +7,17 @@ import { connect } from "react-redux";
 import { handleSliderSeek, handlePlayPauseAction } from "../redux/mediaActions";
 
 export const Seekbar = ({
-  trackLength,
+  tracks,
+  currentIndex,
   currentPosition,
   handleSliderSeek,
   isPlaying,
-  playbackInstance
+  playbackInstance,
 }) => {
+  
+  const { duration } = tracks[currentIndex];
+  // console.log('Seekbar duration :>> ', duration);
+
   // function _pad(n, width, z = 0) {
   //   n = n + "";
   //   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
@@ -38,12 +43,17 @@ export const Seekbar = ({
 
   const onSeek = async () => {
     // console.log("Seekbar onSeek Test");
-    // const valueMilliseconds = value * 1000;
-    // console.log("Seekbar onSeek valueMilliSeconds :>> ", valueMilliseconds);
+    console.log("Seekbar onSeek currentPosition in seconds:>> ", currentPosition);
+    const currentPositionMilliseconds = currentPosition * 1000;
+    console.log("Seekbar onSeek currentPosition in milliseconds :>> ", currentPositionMilliseconds);
     // console.log("Seekbar onSeek playbackInstance:>> ", playbackInstance);
-    // await playbackInstance.playFromPositionAsync(valueMilliseconds);
-    // console.log("Seekbar onSeek value in seconds:>> ", value);
-    // handleSliderSeek(value);
+    await playbackInstance.playFromPositionAsync(currentPositionMilliseconds);
+  };
+
+  const onValueChange = (value) => {
+    const currentPosition = Math.floor(duration*value);
+    // console.log('currentPosition :>> ', currentPosition);
+    handleSliderSeek(currentPosition);
   };
 
   return (
@@ -58,6 +68,7 @@ export const Seekbar = ({
       <Slider
         onSlidingStart={onSlidingStart}
         onSlidingComplete={onSeek}
+        onValueChange={onValueChange}
       />
     </View>
   );
@@ -81,6 +92,8 @@ const mapStateToProps = (state) => ({
   playbackInstance: state.playbackInstance,
   currentPosition: state.currentPosition,
   isPlaying: state.isPlaying,
+  tracks: state.tracks,
+  currentIndex: state.currentIndex
 });
 
 export default connect(mapStateToProps, mapActionsToProps)(Seekbar);
